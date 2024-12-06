69be934a39e247c7c546c8b09acab64d33bea76f
KarthiKeyanZz <pkarthi12k@gmail.com>
2024-12-06 23:31:07 +0530

    Initial commit

---
+++ src/app/data.js
@@ -0,0 +1,123 @@
+/*
+Websites:
+
+- https://github.com/pmndrs/gltfjsx (GLTF JSX for 3D Models)
+- https://lucide.dev/icons/ (Lucide Icons)
+- https://github.com/anuraghazra/github-readme-stats (Github Readme Stats)
+- https://skillicons.dev (Skill Icons to show skills)
+- https://github-readme-streak-stats.herokuapp.com (Github Readme Streak Stats)
+
+:root {
+  --background: 27 27 27;
+  --foreground: 225 225 225;
+  --muted: 115 115 115;
+  --accent: 254 254 91; #FEFE5B
+}
+
+*/
+
+export const projectsData = [
+  {
+    id: 1,
+    name: "Photography-Portfolio-Website 📸",
+    description: "Frame your moments with artistry",
+    date: "2024-11-15",
+    demoLink: "https://github.com/KarthiKeyanZz/Photography-Portfolio-Website",
+  },
+  {
+    id: 2,
+    name: "Blog-App ✍️",
+    description: "Your stories, beautifully shared",
+    date: "2024-10-20",
+    demoLink: "https://github.com/KarthiKeyanZz/Blog-App",
+  },
+  {
+    id: 3,
+    name: "Games-Browsing-Website 🎮",
+    description: "Discover, play, and conquer",
+    date: "2023-10-10",
+    demoLink: "https://github.com/KarthiKeyanZz/Games-Browsing-Website",
+  },
+  {
+    id: 4,
+    name: "Simple-Photo-Editor 🏗️",
+    description: "Monitor heart rate zones",
+    date: "2024-11-30",
+    demoLink: "https://github.com/KarthiKeyanZz/Simple-Photo-Editor",
+  },
+  {
+    id: 5,
+    name: "Hotel-Website 🏨",
+    description: "Experience comfort and elegance",
+    date: "2024-10-12",
+    demoLink: "https://github.com/KarthiKeyanZz/Hotel-Website",
+  },
+  {
+    id: 6,
+    name: "QR-Code-Generator 🟦🔲",
+    description: "Create, scan, and share",
+    date: "2024-09-01",
+    demoLink: "https://github.com/KarthiKeyanZz/QR-Code-Generator",
+  },
+  {
+    id: 7,
+    name: "Company-Portfolio 📑",
+    description: "Showcasing innovation, excellence, and success",
+    date: "2024-12-18",
+    demoLink: "https://github.com/KarthiKeyanZz/CompanyPortfolio",
+  },
+  {
+    id: 8,
+    name: "Generative-AI-Workshop-2.0 🤖💼",
+    description: "Revolutionizing industries with cutting-edge AI solutions",
+    date: "2024-10-05",
+    demoLink: "https://github.com/KarthiKeyanZz/Generative-AI-Megaworkshop-2.0",
+  },
+  {
+    id: 9,
+    name: "Customer-Segmentation 📊",
+    description: "Transform your business with data-driven customer segmentation for targeted success!r",
+    date: "2024-08-22",
+    demoLink: "https://github.com/KarthiKeyanZz/Customer-Segmentation",
+  },
+  {
+    id: 10,
+    name: "Generative-AI-Chatbot 💬🤖",
+    description: "Unlock seamless conversations with our intelligent, next-gen AI chatbot!",
+    date: "2024-11-14",
+    demoLink: "https://github.com/KarthiKeyanZz/GenerativeAI_Workshop",
+  },
+];
+
+export const BtnList = [
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Github",
+    link: "https://github.com/KarthiKeyanZz",
+    icon: "github",
+    newTab: true,
+  },
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/karthikeyan-p-7822a4249/",
+    icon: "linkedin",
+    newTab: true,
+  },
+  {
+    label: "Instagram",
+    link: "https://www.instagram.com/k.a.r.t.h.i.z/",
+    icon: "instagram",
+    newTab: true,
+  },
+  {
+    label: "Resume",
+    link: "/resume.pdf",
+    icon: "resume",
+    newTab: true,
+  },
+];
+
+
---
+++ src/components/about/index.jsx
@@ -0,0 +1,113 @@
+import React from "react";
+import ItemLayout from "./ItemLayout";
+import Link from "next/link";
+
+const AboutDetails = () => {
+  return (
+    <section className="py-20 w-full">
+      <div className="grid grid-cols-12 gap-4 xs:gap-6  md:gap-8 w-full">
+        <ItemLayout
+          className={
+            " col-span-full lg:col-span-8 row-span-4 flex-col items-start"
+          }
+        >
+          <h2 className="text-xl md:text-4xl text-left w-full capitalize">
+            Tech Renaissance Professional
+          </h2>
+          <p className="font-light  text-sm sm:text-md md:text-lg ">
+            In a realm where technology and magic intertwine,
+            I am a seeker of knowledge and a weaver of intricate spells that bridge the
+            mundane and the extraordinary. Gifted with the ability to craft full-stack enchantments,
+            summon algorithms from the depths of machine learning, and shape ethereal data into meaningful insights,
+            I roam the digital landscapes as a technomancer. My arsenal includes the ancient tomes of Python, the elemental
+            forces of HTML5, CSS, and JavaScript, and the powerful artifacts of MongoDB, React.js, and Node.js. Guided by curiosity
+            and a relentless drive to innovate, I aspire to ascend as a master architect of technological marvels
+          </p>
+        </ItemLayout>
+
+        <ItemLayout
+            className={" col-span-full xs:col-span-6 lg:col-span-4 text-accent"}
+        >
+          <p className="font-semibold w-full text-left text-md sm:text-2xl">
+            Education
+          </p>
+        </ItemLayout>
+
+        <ItemLayout
+          className={" col-span-full xs:col-span-8 lg:col-span-4 text-accent"}
+        >
+          <p className="font-semibold w-full text-left text-md sm:text-2xl">
+            Bachelor Of Technology - Artificial Intelligence and Data Science <br/> <sub className="font-semibold text-base">Easa College Of Engineering , CGPA : 80% <br/> 2021 - 2025 </sub>
+          </p>
+        </ItemLayout>
+
+        <ItemLayout
+            className={" col-span-full xs:col-span-8 lg:col-span-4 text-accent"}
+        >
+          <p className="font-semibold w-full text-left text-md sm:text-2xl">
+            Continuous Capacity Building Program (CCBP) 4.0 <br/> <sub className="font-semibold text-base"> Nxtwave Disruptive Technologies <br/> 2021 - 2025 </sub>
+          </p>
+        </ItemLayout>
+
+        <br/>
+
+        <ItemLayout
+          className={"col-span-full sm:col-span-6 md:col-span-4 !p-0"}
+        >
+          <img
+            className="w-full h-auto"
+            src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api/top-langs?username=KarthiKeyanZz&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
+            alt="KarthiKeyan Github Stats"
+            loading="lazy"
+          />
+        </ItemLayout>
+
+        <ItemLayout className={"col-span-full md:col-span-8 !p-0"}>
+          <img
+            className="w-full h-auto"
+            src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api?username=KarthiKeyanZz&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
+            alt="KarthiKeyan Github Lang"
+            loading="lazy"
+          />
+        </ItemLayout>
+
+        <ItemLayout className={"col-span-full"}>
+          <h1 className="text-md sm:text-xs md:text-lg lg:text-4xl text-left w-full capitalize" > Skills and Tools i Use </h1>
+          <br/>
+          <img
+              className="w-full h-auto"
+              src={`https://skillicons.dev/icons?i=appwrite,aws,babel,bootstrap,cloudflare,css,d3,docker,express,figma,firebase,gatsby,git,github,graphql,html,js,jquery,linux,mongodb,mysql,netlify,nextjs,nodejs,npm,python,react,tailwind,threejs,vercel,vite,vscode,yarn`}
+              alt="KarthiKeyanZz Skills"
+              loading="lazy"
+          />
+        </ItemLayout>
+
+        <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
+          <img
+            className="w-full h-auto"
+            src={`${process.env.NEXT_PUBLIC_GITHUB_STREAK_STATS_URL}?user=KarthiKeyanZz&theme=dark&hide_border=true&type=svg&background=EB545400&ring=FEFE5B&currStreakLabel=FEFE5B`}
+            alt="KarthiKeyanZ"
+            loading="lazy"
+          />
+        </ItemLayout>
+
+        <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
+          <Link
+            href="https://github.com/KarthiKeyanZz/Photography-Portfolio-Website"
+            target="_blank"
+            className="w-full"
+          >
+            <img
+              className="w-full h-auto"
+              src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api/pin/?username=KarthiKeyanZz&repo=Photography-Portfolio-Website&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false&description_lines_count=2`}
+              alt="KarthiKeyanZ Streak"
+              loading="lazy"
+            />
+          </Link>
+        </ItemLayout>
+      </div>
+    </section>
+  );
+};
+
+export default AboutDetails;
